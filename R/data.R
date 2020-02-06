04e10661ccd2c0ada2b91794738ae837cb891a9f
cb4ds <connie@aggregate-genius.com>
2020-02-06 04:25:23 +0000

    1.26.8.9001 for testing

--- R/data.R
+++ R/data.R
@@ -7,9 +7,23 @@
 #' @aliases canvasXpress.data canvasXpress.data-package
 NULL
 
-#' Identification of gene expression profiles in cervical cancer
-#'
-#' @format A list of 3 dataframes
+#' Gene Expression Dataset - Cervical Cancer
+#' 
+#' @description This example dataset is taken from the below referenced study.  That study was aimed 
+#' at identification of gene expression profiles in cervical cancer and the role of specific genes
+#' in cervical carcinogenesis.
+#' 
+#' @references Scotto L, Narayan G, Nandula SV, Arias-Pulido H et al. Identification of copy number 
+#' gain and overexpressed genes on chromosome arm 20q by an integrative genomic approach in cervical 
+#' cancer: potential role in progression. Genes Chromosomes Cancer 2008 Sep;47(9):755-65. PMID: 18506748
+#' 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE9750}
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GPL96}
+#' 
+#' @references [HG-U133A] Affymetrix Human Genome U133A Array
+#' 
+#' @format A list of 3 dataframes containing prepared expression, gene, and sample data
+#' 
 "GSE9750"
 
 #' IPF Fibroblast data
--- data-raw/data_prep.R
+++ data-raw/data_prep.R
@@ -1,7 +1,50 @@
 ## Data preparation
+require('dplyr')
+require('tidyr')
+require('tibble')
+require('usethis')
+
 
 exData <- readRDS("data-raw/exData-sm2.rds")
 usethis::use_data(exData, overwrite = TRUE)
 
-GSE9750 <- readRDS("data-raw/GSE9750-sm2.rds")
+
+# GSE9750 preparation from source
+
+y <- readr::read_delim('data-raw/GSE9750_series_matrix.txt.gz', '\t', skip = 73, n_max = 22283) %>% as.data.frame()
+rownames(y) <- y$ID_REF; y$ID_REF <- NULL
+
+z <- readr::read_delim('data-raw/GPL96.annot.gz', '\t', skip = 27) %>%
+    rename(vars        = ID,
+           Description = `Gene title`,
+           Locus       = `Chromosome location`,
+           Symbol      = `Gene symbol`) %>%
+    select(vars, Description, Locus, Symbol) %>%
+    as.data.frame()
+rownames(z) <- z$vars
+
+
+x <- readr::read_delim('data-raw/GSE9750_series_matrix.txt.gz', '\t', skip = 33, n_max = 37) %>%
+    t() %>% as.data.frame(stringsAsFactors = F)
+colnames(x) <- gsub('!', '', x[1, ]); x <- x[-1, ]
+
+x <- x %>%
+    select(Sample_geo_accession, Sample_characteristics_ch1, Sample_description) %>%
+    extract(Sample_characteristics_ch1, "Age", "[A|a]ge +([0-9]+)", remove = F) %>%
+    extract(Sample_characteristics_ch1, "Stage", "[S|s]tage +([A-Z,0-9]+),", remove = F) %>%
+    extract(Sample_characteristics_ch1, "HPV", "(HPV +.*$)", remove = F) %>%
+    mutate(HPV = gsub("HPV", "", HPV),
+           Sample_Type = case_when(grepl("cell line", ignore.case = T, Sample_description) ~ "cell line",
+                                   grepl("dissect", ignore.case = T, Sample_description) ~ "microdissection",
+                                   TRUE ~ "tissue specimen"),
+           Sample_Type = ifelse(grepl('dissect', ignore.case = T, Sample_characteristics_ch1), 'microdissection', Sample_Type),
+           Cell_Type = case_when(grepl('squamous', ignore.case = T, Sample_characteristics_ch1) ~ 'squamous cell carcinoma', 
+                                 grepl('adenocarcinoma', ignore.case = T, Sample_characteristics_ch1) ~ 'adenocarcinoma',
+                                 grepl('normal', ignore.case = T, Sample_characteristics_ch1) ~ 'normal')) %>%
+    select(Sample_geo_accession, Sample_Type, Cell_Type, Stage, Age, HPV) %>%
+    as.data.frame()
+rownames(x) <- x$Sample_geo_accession; x$Sample_geo_accession <- NULL
+
+
+GSE9750 <- list(x = x, y = y, z = z)           
 usethis::use_data(GSE9750, overwrite = TRUE)
